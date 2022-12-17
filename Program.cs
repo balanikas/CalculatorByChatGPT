7dcf2c5b34aaf39aeb27dfb28daaed554f933aa4
balanikas <c_balanikas@hotmail.com>
2022-12-17 13:50:02 +0100

    ai: write an empty program.cs with dotnet 7. syntax that imports standard usings

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your code here
+           
+        }
+    }
+}
