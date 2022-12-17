8a75814a61f29aa22c21f442747caf454cb1dbe5
balanikas <c_balanikas@hotmail.com>
2022-12-17 13:51:16 +0100

    ai: check that the main method arguments are only 2 , and they are numeric

--- Program.cs
+++ Program.cs
@@ -9,6 +9,17 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Error: The program requires exactly two numeric arguments.");
+                return;
+            }
+
+            if (!int.TryParse(args[0], out int x) || !int.TryParse(args[1], out int y))
+            {
+                Console.WriteLine("Error: The arguments must be numeric.");
+                return;
+            }
             // Your code here
            
         }
